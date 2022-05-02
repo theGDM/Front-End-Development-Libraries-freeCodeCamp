57f263fa8e1319a1d96ca798f3eedbf06ba877b3
theGDM <gyandeepmehra370@gmail.com>
2022-05-02 20:16:55 +0530

    Create Add Inline Styles in React.js

---
+++ React/Add Inline Styles in React.js
@@ -0,0 +1,17 @@
+const styles = {
+  color : "purple",
+  border : "2px solid purple", 
+  fontSize: 40
+};
+
+// Change code above this line
+class Colorful extends React.Component {
+
+  render() {
+    // Change code below this line
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+    // Change code above this line
+  }
+};
