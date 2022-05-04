78967e0fe72750a6e4b0f230e6834ece7a73f56d
theGDM <gyandeepmehra370@gmail.com>
2022-05-04 16:56:14 +0530

    Create Give Sibling Elements a Unique Key Attribute.js

---
+++ React/Give Sibling Elements a Unique Key Attribute.js
@@ -0,0 +1,22 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((frame, idx) => {
+    return (<li key={idx}>{frame}</li>)
+  }); // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
