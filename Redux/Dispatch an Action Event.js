822e45b55ef6220c51604f7ae0573a22c09fb043
theGDM <gyandeepmehra370@gmail.com>
2022-05-27 08:45:04 +0530

    Create Dispatch an Action Event.js

---
+++ Redux/Dispatch an Action Event.js
@@ -0,0 +1,15 @@
+const store = Redux.createStore(
+  (state = {login: false}) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Dispatch the action here:
+store.dispatch(loginAction());
+
+const currentState = store.getState();
+console.log(currentState.login);
