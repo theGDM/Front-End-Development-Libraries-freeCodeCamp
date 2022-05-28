45c2c036d3f1b691df85e515334e791061541619
theGDM <gyandeepmehra370@gmail.com>
2022-05-28 18:55:25 +0530

    Create Use the Spread Operator on Arrays.js

---
+++ Redux/Use the Spread Operator on Arrays.js
@@ -0,0 +1,23 @@
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+  switch(action.type) {
+    case 'ADD_TO_DO':
+      // Don't mutate state here or the tests will fail
+      return [...state, action.todo]
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: 'ADD_TO_DO',
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
+// store.dispatch(addToDo("Washing the cloths"));
+// console.log(store.getState());
+
+// store.dispatch(addToDo("Go to Market"));
+// console.log(store.getState());
